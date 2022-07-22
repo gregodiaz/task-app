4dfb64e9057465f0c6226fba49b68fefb9ba7e10
AgusPk <agustinperkins@hotmail.com>
2022-07-22 18:08:11 -0300

    added commment

--- src/components/DefaultTemplate/index.jsx
+++ src/components/DefaultTemplate/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../NavBar";
 
+//TODO medio pija estar escribiendo props en todos lados, que tal algo tipo {children}?
 export default function DefaultTemplate(props) {
   return (
     <div>
